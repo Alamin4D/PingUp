2feb2629bcd67e7160afb09bcf4a1d8dc5f12b7a
Alamin4D <alaminahmed79000@gmail.com>
2025-09-04 12:50:56 +0600

    empt-file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Feed from './pages/Feed';
+import Messages from './pages/Messages';
+import ChatBox from './pages/ChatBox';
+import Connections from './pages/Connections';
+import Discover from './pages/Discover';
+import Profile from './pages/Profile';
+import CreatePost from './pages/CreatePost';
+import { useUser } from '@clerk/clerk-react'
+import Layout from './pages/Layout';
 
 const App = () => {
+  const { user } = useUser();
   return (
-    <div className='text-5xl font-bold text-red-500'>Hello world...!</div>
+    <>
+      <Routes>
+        <Route path='/' element={!user ? <Login /> : <Layout />}>
+          <Route index element={<Feed />} />
+          <Route path='messages' element={<Messages />} />
+          <Route path='messages/:userId' element={<ChatBox />} />
+          <Route path='connections' element={<Connections />} />
+          <Route path='discover' element={<Discover />} />
+          <Route path='profile' element={<Profile />} />
+          <Route path='profile/:Id' element={<Profile />} />
+          <Route path='create-post' element={<CreatePost />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
 
